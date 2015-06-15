899e983a7523f53caa9ac6d1c83f6dac48addddb
Svyatoslav Shaforenko <Slavik-1988@mail.ru>
2015-06-15 19:11:01 +0300

    - Project created;

---
+++ Hawkist/Hawkist/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Hawkist
+//
+//  Created by Svyatoslav Shaforenko on 6/15/15.
+//  Copyright (c) 2015 TecSynt Solutions. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Hawkist/HawkistTests/HawkistTests.m
@@ -0,0 +1,40 @@
+//
+//  HawkistTests.m
+//  HawkistTests
+//
+//  Created by Svyatoslav Shaforenko on 6/15/15.
+//  Copyright (c) 2015 TecSynt Solutions. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface HawkistTests : XCTestCase
+
+@end
+
+@implementation HawkistTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
