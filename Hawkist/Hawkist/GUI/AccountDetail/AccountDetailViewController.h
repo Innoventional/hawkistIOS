44993fd9d9e00c9c8c5217a44e4d66e9cd7808e6
Anton Makarov <N80939733040@gmail.com>
2015-06-22 12:48:07 +0300

    AccountView_modify

--- Hawkist/Hawkist/GUI/AccountDetail/AccountDetailViewController.h
+++ Hawkist/Hawkist/GUI/AccountDetail/AccountDetailViewController.h
@@ -9,14 +9,22 @@
 #import <UIKit/UIKit.h>
 #import "HWBaseViewController.h"
 
-@interface AccountDetailViewController : HWBaseViewController
+@interface AccountDetailViewController : HWBaseViewController <UIActionSheetDelegate,UINavigationControllerDelegate,UIImagePickerControllerDelegate>
 @property (weak, nonatomic) IBOutlet UITextField *txtEmail;
+
 @property (weak, nonatomic) IBOutlet UIScrollView *scrollView;
+
+- (IBAction)tapImage:(id)sender;
+
 @property (weak, nonatomic) IBOutlet UIButton *checkbox1;
 
 @property (weak, nonatomic) IBOutlet UIButton *checkBox2;
+@property (weak, nonatomic) IBOutlet UITextView *txtAboutMe;
 
 - (IBAction)checkBox:(id)sender;
+
+@property (weak, nonatomic) IBOutlet UIView *wwwww;
+- (IBAction)tapView:(id)sender;
 
 @property (weak, nonatomic) IBOutlet UIButton *btnContinue;
 @property (weak, nonatomic) IBOutlet UITextField *txtUserName;
--- Hawkist/Hawkist/GUI/AccountDetail/AccountDetailViewController.m
+++ Hawkist/Hawkist/GUI/AccountDetail/AccountDetailViewController.m
@@ -7,10 +7,15 @@
 //
 
 #import "AccountDetailViewController.h"
+#import <MobileCoreServices/MobileCoreServices.h>
+#import "UIView+Extensions.h"
+#import "Masonry.h"
 
 @interface AccountDetailViewController ()
 @property (nonatomic,strong) UIView* accountDetailView;
 @property (nonatomic, assign) BOOL* isContinueEnable;
+
+
 @end
 
 @implementation AccountDetailViewController
@@ -37,10 +42,108 @@
     
     self.isContinueEnable = false;
     
+    [self registerForKeyboardNotifications];
     
     
-
 }
+
+//
+//// Call this method somewhere in your view controller setup code.
+//- (void)registerForKeyboardNotifications
+//{
+//    [[NSNotificationCenter defaultCenter] addObserver:self
+//                                             selector:@selector(keyboardWasShown:)
+//                                                 name:UIKeyboardDidShowNotification object:nil];
+//    [[NSNotificationCenter defaultCenter] addObserver:self
+//                                             selector:@selector(keyboardWillBeHidden:)
+//                                                 name:UIKeyboardWillHideNotification object:nil];
+//}
+//
+//// Called when the UIKeyboardDidShowNotification is sent.
+//- (void)keyboardWasShown:(NSNotification*)aNotification
+//{
+//    NSDictionary* info = [aNotification userInfo];
+//    CGSize kbSize = [[info objectForKey:UIKeyboardFrameBeginUserInfoKey] CGRectValue].size;
+//    UIEdgeInsets contentInsets = UIEdgeInsetsMake(0.0, 0.0, kbSize.height, 0.0);
+//    _scrollView.contentInset = contentInsets;
+//    _scrollView.scrollIndicatorInsets = contentInsets;
+//    
+//    // If active text field is hidden by keyboard, scroll it so it's visible
+//    // Your application might not need or want this behavior.
+//    CGRect aRect = self.view.frame;
+//    aRect.size.height -= kbSize.height;
+//    if (!CGRectContainsPoint(aRect, activeField.frame.origin) ) {
+//        CGPoint scrollPoint = CGPointMake(0.0, activeField.frame.origin.y-kbSize.height);
+//        [_scrollView setContentOffset:scrollPoint animated:YES];
+//    }
+//}
+//
+//// Called when the UIKeyboardWillHideNotification is sent
+//- (void)keyboardWillBeHidden:(NSNotification*)aNotification
+//{
+//    UIEdgeInsets contentInsets = UIEdgeInsetsZero;
+//    _scrollView.contentInset = contentInsets;
+//    _scrollView.scrollIndicatorInsets = contentInsets;
+//}
+
+
+
+
+- (BOOL)textFieldShouldReturn:(UITextField *)textField
+{
+    [textField resignFirstResponder];
+    return YES;
+}
+
+
+- (void) registerForKeyboardNotifications
+{
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(adjustKeyboardFrame:)
+                                                 name:UIKeyboardWillShowNotification
+                                               object:nil];
+    
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(hideKeyboardFrame:)
+                                                 name:UIKeyboardWillHideNotification
+                                               object:nil];
+    
+    
+}
+#pragma mark -
+#pragma mark Keyboard
+
+
+- (void) adjustKeyboardFrame: (NSNotification*) notification
+{
+    BOOL willHide = [notification.name isEqualToString: UIKeyboardWillHideNotification];
+    
+    CGRect keyboardFrame = [notification.userInfo[UIKeyboardFrameEndUserInfoKey] CGRectValue];
+
+    CGFloat keyboardHeight = (CGRectGetMinY(keyboardFrame) < self.view.height) ? CGRectGetHeight(keyboardFrame) : 0.0f;
+    
+    CGFloat bottomOffset = willHide ? 0.0f : keyboardHeight;
+//
+//    [self.scrollView setContentInset: UIEdgeInsetsMake(0, 0, bottomOffset, 0)];
+//    NSLog(@"%f-key",bottomOffset);
+
+     CGRect newRect = CGRectMake(0, -bottomOffset, self.view.frame.size.width, self.view.frame.size.height);
+    
+    self.view.frame = newRect;
+}
+
+- (void) hideKeyboardFrame: (NSNotification*) notification
+{
+    CGRect newRect = CGRectMake(0,0, self.view.frame.size.width, self.view.frame.size.height);
+    
+    self.view.frame = newRect;
+//    
+//    [self.scrollView setContentInset: UIEdgeInsetsMake(0, 0, 0, 0)];
+ 
+}
+
+
+
 
 - (UIStatusBarStyle)preferredStatusBarStyle
 {
@@ -84,4 +187,72 @@
 }
 
 
+
+- (IBAction)tapImage:(id)sender {
+    UIActionSheet* popup = [[UIActionSheet alloc] initWithTitle: @""
+                                                       delegate: self
+                                              cancelButtonTitle: @"Cancel"
+                                         destructiveButtonTitle: nil
+                                              otherButtonTitles: @"Take Photo", @"Choose Photo", nil];
+    popup.tag = 1;
+    [popup showInView: [UIApplication sharedApplication].keyWindow];
+    [self.txtEmail resignFirstResponder];
+}
+
+- (void)    actionSheet: (UIActionSheet*) popup
+   clickedButtonAtIndex: (NSInteger) buttonIndex
+{
+    switch (popup.tag)
+    {
+        case 1:
+        {
+            switch (buttonIndex)
+            {
+                case 0:
+                    [self takePhoto: YES
+                        fromGallery: NO];
+                    break;
+                    
+                case 1:
+                    [self takePhoto: YES
+                        fromGallery: YES];
+                    break;
+                    
+                default:
+                    break;
+            }
+            break;
+        }
+        default:
+            break;
+
+}
+}
+
+- (void) takePhoto: (BOOL)photo
+       fromGallery:(BOOL)gallery
+{
+    UIImagePickerControllerSourceType source = gallery ? UIImagePickerControllerSourceTypePhotoLibrary :
+    UIImagePickerControllerSourceTypeCamera;
+    
+    if ([UIImagePickerController isSourceTypeAvailable: source])
+    {
+        UIImagePickerController* picker = [[UIImagePickerController alloc] init];
+        
+        picker.delegate      = self;
+        picker.allowsEditing = YES;
+        if(photo)
+            picker.mediaTypes    = @[(NSString*) kUTTypeImage];
+        picker.sourceType    = source;
+        
+        [self presentViewController: picker animated: YES completion: NULL];
+    }
+}
+
+- (IBAction)tapView:(id)sender {
+    [self.txtEmail resignFirstResponder];
+    [self.txtUserName resignFirstResponder];
+    [self.txtAboutMe resignFirstResponder];
+    
+}
 @end
