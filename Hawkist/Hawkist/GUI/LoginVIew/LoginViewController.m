f70f7a14b0be8313b89a99f287f1f861182cb5f0
Anton Makarov <N80939733040@gmail.com>
2015-06-26 02:53:54 +0300

    Add BtnNext To PhonePad

--- Hawkist/Hawkist/GUI/LoginVIew/LoginViewController.m
+++ Hawkist/Hawkist/GUI/LoginVIew/LoginViewController.m
@@ -134,8 +134,45 @@
     
     _txtMobileNum.text = _engine.number;
     _txtPin.text = _engine.pin;
-
+    
+    
+    
+    
+    
+    UIToolbar* numberToolbar = [[UIToolbar alloc]initWithFrame:CGRectMake(0, 0, 320, 50)];
+    numberToolbar.barStyle = UIBarStyleDefault;
+    numberToolbar.items = [NSArray arrayWithObjects:
+                           [[UIBarButtonItem alloc]initWithBarButtonSystemItem:UIBarButtonSystemItemFlexibleSpace target:nil action:nil],
+                           [[UIBarButtonItem alloc]initWithTitle:@"Next" style:UIBarButtonItemStyleDone target:self action:@selector(nextToPin)],
+                           nil];
+    [numberToolbar sizeToFit];
+    
+    UIToolbar* numberToolbar2 = [[UIToolbar alloc]initWithFrame:CGRectMake(0, 0, 320, 50)];
+    numberToolbar2.barStyle = UIBarStyleDefault;
+    numberToolbar2.items = [NSArray arrayWithObjects:
+                           [[UIBarButtonItem alloc]initWithBarButtonSystemItem:UIBarButtonSystemItemFlexibleSpace target:nil action:nil],
+                           [[UIBarButtonItem alloc]initWithTitle:@"Next" style:UIBarButtonItemStyleDone target:self action:@selector(nextTo)],
+                           nil];
+    [numberToolbar2 sizeToFit];
+    
+    _txtMobileNum.inputAccessoryView = numberToolbar;
+    _txtPin.inputAccessoryView = numberToolbar2;
+    
+    
 }
+    
+- (void) nextToPin
+{
+    [_txtMobileNum resignFirstResponder];
+    [_txtPin becomeFirstResponder];
+}
+
+- (void) nextTo
+{
+    [_txtPin resignFirstResponder];
+    [self btnSignInMobile:self];
+}
+
 
 - (void) registerForKeyboardNotifications
 {
