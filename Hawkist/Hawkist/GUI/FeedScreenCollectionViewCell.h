42258569cf4fc77571ab795717cc7272dda40c10
Evgen Bootcov <ebootcov88@gmail.com>
2015-07-02 13:51:30 +0300

    no message

--- Hawkist/Hawkist/GUI/FeedScreenCollectionViewCell.h
+++ Hawkist/Hawkist/GUI/FeedScreenCollectionViewCell.h
@@ -10,30 +10,30 @@
 
 @interface FeedScreenCollectionViewCell : UICollectionViewCell
 
-//@property (strong, nonatomic) IBOutlet UIImageView *avatarImage;
-//
-//@property (strong, nonatomic) IBOutlet UILabel *userNameLable;
-//
+@property (strong, nonatomic) IBOutlet UIImageView *avatarImage;
+
+@property (strong, nonatomic) IBOutlet UILabel *userNameLable;
+
 @property (strong, nonatomic) IBOutlet UILabel *timeLable;
-//
-//@property (strong, nonatomic) IBOutlet UIImageView *itemImage;
-//
-//@property (strong, nonatomic) IBOutlet UILabel *reductionLable;
-//
-//@property (strong, nonatomic) IBOutlet UILabel *sellItemLabel;
-//
-//@property (strong, nonatomic) IBOutlet UILabel *starsLable;
-//
-//@property (strong, nonatomic) IBOutlet UILabel *opinionLable;
-//
-//@property (strong, nonatomic) IBOutlet UIImageView *starImage;
-//
-//@property (strong, nonatomic) IBOutlet UIImageView *speechImage;
-//
-//@property (strong, nonatomic) IBOutlet UILabel *consoleNameLable;
-//
-//@property (strong, nonatomic) IBOutlet UILabel *oldPriceLable;
-//@property (strong, nonatomic) IBOutlet UILabel *currentPriceLable;
+
+@property (strong, nonatomic) IBOutlet UIImageView *itemImage;
+
+@property (strong, nonatomic) IBOutlet UILabel *reductionLable;
+
+@property (strong, nonatomic) IBOutlet UILabel *sellItemLabel;
+
+@property (strong, nonatomic) IBOutlet UILabel *starsLable;
+
+@property (strong, nonatomic) IBOutlet UILabel *opinionLable;
+
+@property (strong, nonatomic) IBOutlet UIImageView *starImage;
+
+@property (strong, nonatomic) IBOutlet UIImageView *speechImage;
+
+@property (strong, nonatomic) IBOutlet UILabel *consoleNameLable;
+
+@property (strong, nonatomic) IBOutlet UILabel *oldPriceLable;
+@property (strong, nonatomic) IBOutlet UILabel *currentPriceLable;
 
 
 
