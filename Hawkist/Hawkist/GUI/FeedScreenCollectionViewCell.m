fdb4eeea46b7204f03bc1d8fa81d5c564f1887d4
Evgen Bootcov <ebootcov88@gmail.com>
2015-07-10 17:11:37 +0300

    - 0% fixed;

--- Hawkist/Hawkist/GUI/FeedScreenCollectionViewCell.m
+++ Hawkist/Hawkist/GUI/FeedScreenCollectionViewCell.m
@@ -30,10 +30,10 @@
     
     
     //self.skidkaTextfield.text = self.item.discount;
-    if (self.item.discount) {
-        self.skidkaTextfield.text = self.item.discount;
+    if (self.item.discount == nil || [self.item.discount isEqualToString: @"0"]) {
+        self.skidkaTextfield.text = @"1";
     } else {
-        self.skidkaTextfield.text = @"0";
+        self.skidkaTextfield.text = self.item.discount;
     }
     self.sellItemLabel.text = self.item.title;
     self.currentPriceLable.text = self.item.selling_price;
